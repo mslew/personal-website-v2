9dcc7c8304b9eacb8216f5b03333fa3b49baf998
mslew <mlew3223@gmail.com>
2022-12-20 15:04:20 -0600

    trying to figure out the logo.

---
+++ src/lib/sections/Intro.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import Logo from "../components/icons/Logo.svelte"
+</script>
+
+<i><Logo /></i>
+
+<style>
+    i{
+        size: 2rem;
+        color: red;
+        display: flex; 
+        align-items: center; 
+        justify-content: center; 
+        text-align: center;  
+    }
+</style>
