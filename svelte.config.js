34be25c4320733e17f36b9c35403244441dcd407
mslew <mlew3223@gmail.com>
2023-01-03 13:59:22 -0600

    added cdif.md

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,20 @@
 import preprocess from "svelte-preprocess";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { mdsvex } from 'mdsvex';
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
+  extensions: ['.svelte', '.md'],
   preprocess: [
     vitePreprocess(),
     preprocess({
       scss: {
         prependData: '@use "src/scss/variables.scss" as *;',
       },
-    }),
+    },
+    mdsvex({
+      extensions: ['.md']
+    }),),
   ],
 };
