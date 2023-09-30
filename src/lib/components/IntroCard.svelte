1e6bfadefe467920c27b7f6f06f57f038f821dbb
mslew <mlew3223@gmail.com>
2023-09-30 01:20:06 -0500

    smaller screen stuff oncemore

--- src/lib/components/IntroCard.svelte
+++ src/lib/components/IntroCard.svelte
@@ -25,19 +25,19 @@
 
 </script>
 
-<div class="h-screen w-full grid grid-cols-3 grid-rows-2 justify-items-center place-items-center tall:portrait:mb-14 pt-12">
-    <div class="row-span-2 col-span-2 -2xl:col-span-3">
+<div class="h-screen w-full grid grid-cols-4 grid-rows-2 justify-items-center place-items-center">
+    <div class="row-span-2 col-span-3 -2xl:col-span-4 -xl:w-2/3 -lg:w-3/4 -md:w-full sm:mt-32 md:mt-0">
         {#if nameVisible}
-        <p transition:fly="{{ y: -100, duration: 100 }}" class="text-2xl p-6 dark:text-purple-900 text-purple-600 -lg:text-xl -sm:pb-0 wide:landscape:pb-0">Hi, my name is</p>
+        <p transition:fly="{{ y: -100, duration: 100 }}" class="text-2xl pl-6 pt-4 dark:text-purple-900 text-purple-600 -lg:text-xl sm:pb-0">Hi, my name is</p>
         {/if}
         {#if nameVisible}
-        <h1 transition:fly="{{ y: -100, duration: 100 }}" class="text-7xl p-6 -lg:text-4xl -sm:pb-0 wide:landscape:pb-0">Maximus Lewis.</h1>
+        <h1 transition:fly="{{ y: -100, duration: 100 }}" class="text-7xl pl-6 pt-4 -lg:text-4xl sm:pb-0">Maximus Lewis.</h1>
         {/if}
         {#if subtitleVisible}
-        <h1 transition:fly="{{ y: -100, duration: 100 }}" class="text-6xl p-6 -lg:text-4xl -sm:pb-0 wide:landscape:pb-0">Software Engineer</h1>
+        <h1 transition:fly="{{ y: -100, duration: 100 }}" class="text-6xl pl-6 pt-4 -lg:text-4xl sm:pb-0">Software Engineer</h1>
         {/if}
         {#if contentVisible}
-        <p transition:fly="{{ y: -100, duration: 100 }}" class="text-2xl p-6 dark:text-purple-900 text-purple-600 -lg:text-xl">I am a recent Lewis University Computer Science graduate.</p>
+        <p transition:fly="{{ y: -100, duration: 100 }}" class="text-2xl pl-6 pt-4 dark:text-purple-900 text-purple-600 -lg:text-xl">I am a recent Lewis University Computer Science graduate.</p>
         {/if}
     </div>
 </div>
