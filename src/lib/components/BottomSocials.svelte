9089584a78a718cd120a6886898f3b20958ee7c1
mslew <mlew3223@gmail.com>
2023-10-08 01:32:24 -0500

    update some things

--- src/lib/components/BottomSocials.svelte
+++ src/lib/components/BottomSocials.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
+    import {GitHubURL, LinkedInURL, XTwitterURL, InstagramURL} from '../../config'
     import GitHub from "./icons/GitHub.svelte";
     import LinkedIn from "./icons/LinkedIn.svelte";
-    import Handshake from "./icons/Handshake.svelte";
+    import X from "./icons/X.svelte";
     import Instagram from "./icons/Instagram.svelte";
 </script>
 
 <div class="flex flex-row items-center gap-4 mb-4">
     <div class="border dark:border-white border-black ml-2 -md:pr-12"></div>
-    <a class="item" href="https://github.com/mslew" target = "_blank" rel="noreferrer" aria-label="GitHub" title="GitHub"><GitHub class="icon" height={30}/></a>
-    <a class="item" href = "https://www.linkedin.com/in/maximus-lewis-50ba47203/" target = "_blank" rel="noreferrer" aria-label="LinkedIn" title="LinkedIn"><LinkedIn class="icon" height={30}/></a>
-    <a class="item" href = "https://lewisu.joinhandshake.com/users/20583963" target = "_blank" rel="noreferrer" aria-label="Handshake" title="Handshake"><Handshake class="icon" height={30}/></a>
-    <a class="item" href = "https://www.instagram.com/maxslewis/" target = "_blank" rel="noreferrer" aria-label="Instagram" title="Instagram"><Instagram class="icon" height={30}/></a>
+    <a class="item" href={GitHubURL.url} target = "_blank" rel="noreferrer" aria-label="GitHub" title="GitHub"><GitHub class="icon" height={30}/></a>
+    <a class="item" href={LinkedInURL.url} target = "_blank" rel="noreferrer" aria-label="LinkedIn" title="LinkedIn"><LinkedIn class="icon" height={30}/></a>
+    <a class="item" href={XTwitterURL.url} target = "_blank" rel="noreferrer" aria-label="Handshake" title="Handshake"><X class="icon" height={30}/></a>
+    <a class="item" href={InstagramURL.url} target = "_blank" rel="noreferrer" aria-label="Instagram" title="Instagram"><Instagram class="icon" height={30}/></a>
     <div class="border dark:border-white border-black mr-2 -md:pr-12 "></div>
 </div>
 
