7d611e63b1b90e2b2f4307d212848f575ad05b73
mslew <mlew3223@gmail.com>
2024-09-23 00:36:19 -0500

    spacing on about section for smaller screens

--- src/lib/sections/About.svelte
+++ src/lib/sections/About.svelte
@@ -2,29 +2,30 @@
     import AboutPicture from '../components/AboutPicture.svelte';
 </script>
 
-<section class="about h-full pl-44 pr-44 w-full grid grid-cols-2 grid-rows-1 justify-items-center gap-10 -lg:justify-items-center -lg:place-items-center" id="about">
-    <div class="reveal ml-10 -lg:col-span-2 -xl:ml-0">
-        <div class="flex flex-row items-center gap-4">
-            <p class="text-3xl dark:text-purple-900 p-1 object-bottom text-purple-600 -lg:text-xl -md:text-xl">01.</p>
-            <p class="text-2xl -lg:text-lg">About Me</p>
-            <div class="border dark:border-white border-black ml-2 pr-72 -xl:pr-40 -lg:pr-36 -sm:pr-32"></div>
-        </div>
-        <div class="text-xl flex flex-col gap-4 -xl:text-lg -md:text-sm">
-            <p>
-                Growing up in Lockport, Illinois, I have always enjoyed technology. 
-                In high school, I decided to take a couple of coding classes because it would be fun. 
-                Who would have thought the impact it would have had on my life? 
-                I decided to major in Computer Science!
-            </p>
-            <p> 
-                Studying Computer Science at Lewis University 
-                has been a mentally challenging, but equally intellectually stimulating time of my life. 
-                I have enjoyed my studies and I am extremely excited to be a recent graduate and eager to start my career!
-            </p>
-            <p>
-                Here are a few technologies that I know and some I have been working with recently: 
-            </p>
-        </div>
+<section class="about h-full w-full flex flex-row justify-center -md:mb-32" id="about">
+    <div class="border-red-400 h-full w-3/4 grid grid-cols-2 grid-rows-1 justify-items-center gap-10 -lg:justify-items-center -lg:place-items-center -md:w-10/12">
+        <div class="reveal ml-10 -lg:col-span-2 -xl:ml-0">
+            <div class="flex flex-row items-center gap-4">
+                <p class="text-3xl dark:text-purple-900 p-1 object-bottom text-purple-600 -lg:text-xl -md:text-xl">01.</p>
+                <p class="text-2xl -lg:text-lg">About Me</p>
+                <div class="border dark:border-white border-black ml-2 pr-72 -xl:pr-40 -lg:pr-36 -sm:pr-32"></div>
+            </div>
+            <div class="text-xl flex flex-col gap-4 -xl:text-lg -md:text-sm">
+                <p>
+                    Growing up in Lockport, Illinois, I have always enjoyed technology. 
+                    In high school, I decided to take a couple of coding classes because it would be fun. 
+                    Who would have thought the impact it would have had on my life? 
+                    I decided to major in Computer Science!
+                </p>
+                <p> 
+                    Studying Computer Science at Lewis University 
+                    has been a mentally challenging, but equally intellectually stimulating time of my life. 
+                    I have enjoyed my studies and I am extremely excited to be a recent graduate and eager to start my career!
+                </p>
+                <p>
+                    Here are a few technologies that I know and some I have been working with recently: 
+                </p>
+            </div>
         <div class="grid grid-cols-2 grid-rows-1 justify-items-center place-items-center text-xl mt-6 -lg:text-lg -md:text-base">
             <div class="">
                 <ul class="list-square">
@@ -41,11 +42,12 @@
                     <li>Sass</li>
                     <li>Tailwind CSS</li>
                 </ul>
+            </div>
+            </div>
         </div>
-    </div>
-    </div>
-    <div class="reveal flex flex-col items-center xl:ml-10 lg:ml-0 -lg:mr-10 -md:mr-0 -lg:col-span-2">
-        <AboutPicture />
+        <div class="reveal flex flex-col items-center xl:ml-10 lg:ml-0 -lg:mr-10 -md:mr-0 -lg:col-span-2">
+            <AboutPicture />
+        </div>
     </div>
 </section>
 
