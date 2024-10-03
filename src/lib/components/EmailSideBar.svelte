52284f9578897ba6af4d22eccea3caffe4ce1bb4
mslew <mlew3223@gmail.com>
2024-10-03 11:39:10 -0500

    fix line alignment for email

--- src/lib/components/EmailSideBar.svelte
+++ src/lib/components/EmailSideBar.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-col items-center">
     <a class="email" href={"mailto:" + Email.name} target = "_blank" aria-label="Email" title="Email">{Email.name}</a>
-    <div class="border dark:border-white h-full mt-2 mr-2 border-black pb-14 -xl:pb-10"></div>
+    <div class="border dark:border-white h-full mt-2 border-black pb-14 -xl:pb-10"></div>
 </div>
 
 <style lang="postcss">
