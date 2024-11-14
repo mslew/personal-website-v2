19714d49fdfec6c3860f8b46343bd201c78f1a4b
mslew <mlew3223@gmail.com>
2024-11-14 15:14:02 -0600

    line height

--- src/lib/components/EmailSideBar.svelte
+++ src/lib/components/EmailSideBar.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-col items-center">
     <a class="email dark:hover:text-purple-900 pb-3 hover:text-purple-600 dark:text-white text-black transition ease-in-out hover:-translate-y-1 duration-300" href={"mailto:" + Email.name} target = "_blank" aria-label="Email" title="Email">{Email.name}</a>
-    <div class="border dark:border-white mt-2 border-black h-10 -xl:h-6"></div>
+    <div class="border dark:border-white border-black h-12 -xl:h-8"></div>
 </div>
 <!--NOTE: This is a temporary fix for the tailwindcss bug. @apply does not work with the class attribute in svelte-5-->
 
